ea6d6da3b0decd6429cd5679ad52918c71438f36
Dipanjan <dipanjan.haldar44@gmail.com>
2018-10-23 13:01:45 -0400

    adding the array rotation program

---
+++ ProgramPractise/rotateArray.py
@@ -0,0 +1,23 @@
+def leftShift (inputList, noOfMoves):
+    count=0
+    while count < noOfMoves:
+        item=inputList.pop(0)
+        inputList.append(item)
+        count=count+1
+    return inputList
+
+def rightShift (inputList, noOfMoves):
+    count=0
+    while count < noOfMoves:
+        item=inputList.pop()
+        inputList.insert(0,item)
+        count=count+1
+    return inputList
+
+def main():
+    inputList=[1,2,3,4,5,6,7,8,9]
+    noOfMoves=4
+    print(leftShift(inputList,noOfMoves))
+    print (rightShift (inputList, noOfMoves))
+
+if __name__ == "__main__": main ()
