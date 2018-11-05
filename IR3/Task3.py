aa4e2a766df814ea54fdb4db15a0f3f1532f5a22
Dipanjan Haldar <35041418+dipanjan44@users.noreply.github.com>
2018-11-05 10:57:45 -0500

    Add files via upload

--- IR3/Task3.py
+++ IR3/Task3.py
@@ -2,13 +2,13 @@ import os
 
 
 # Task3
-corpus_location = "/Users/dipanjan/PycharmProjects/Python/IR3/TestFiles/"
+corpus_location = "/Users/dipanjan/PycharmProjects/Python/IR3/ProcessedFiles/"
 slider = 5  # value of K
-term1 = "jump"  # the first search term
-term2 = "Billion"  # the second search term
+term1 = "carbon"  # the first search term
+term2 = "emission"  # the second search term
 
 
-# Read the contents of the corpus to create an index
+# Generate the file list for unigrams based on slider(k) criteria match
 
 def generate_positional_unigram_file_list (corpus_location, slider, term1, term2):
     document_set = set ()
@@ -18,23 +18,30 @@ def generate_positional_unigram_file_list (corpus_location, slider, term1, term2
         current_file = open (corpus_location + filename)
         tokens = get_tokens (current_file)
         current_file.close ()
+        # Get the positional unigram for the current file
         corpus_map_with_positions = build_positional_unigram (tokens, filename)
         # Convert all the tokens  and search terms to lower case to handle case-insensitivity
         corpus_map_with_positions = {k.lower (): v for k, v in corpus_map_with_positions.items ()}
         term1=term1.lower()
         term2=term2.lower()
 
+
+        # check whether the given terms occur in the positional unigram of this file)
         if (term1 in corpus_map_with_positions.keys () and term2 in corpus_map_with_positions.keys ()):
+            # if both terms are present flatten the map for the terms to get the position lists
             term1_position_list = corpus_map_with_positions[term1].__getitem__ (2)
             term2_position_list = corpus_map_with_positions[term2].__getitem__ (2)
+            # compare each element of the position list to check where there is diff is within or equal to the slider(k)
             for element in term1_position_list:
                 for item in term2_position_list:
+                    # check the diff for elements and compare with slider value
                     if abs (element - item) <= slider:
                         file_key = filename.split (".")[0]
+                        # if it matches the criteria, add the documentId and position to the set
                         document_set.add (str (file_key) + " : " + " { " + str (term1) + " : " + str (
                             term1_position_list) + " } ," + " { " + str (term2) + " : " + str (
                             term2_position_list) + " } ")
-
+    # write each element of the set to the output file
     for entry in document_set:
         outfile.write (str (entry) + "\n")
 
@@ -46,7 +53,7 @@ def get_tokens (file):
     return tokens
 
 
-# n-grams with positions
+# Build unigram with positions for each file
 def build_positional_unigram (tokens, filename):
     corpus_map_with_positions = {}
     for index, token in enumerate (tokens):
